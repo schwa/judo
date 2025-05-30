f6b22c7b207977fddb64be66861a5f0abc1da166
Jonathan Wight <jwight@mac.com>
2025-05-30 09:44:40 -0700

    Add Everything package and update content view
    - Integrated the "Everything" Swift package into the project to enhance functionality.
    - Simplified `ContentView` by updating the text and removing redundant UI components.
    - Removed unnecessary app sandbox entitlements.
    - Introduced a new `Repository` class with basic logging functionality.
    - Updated `Package.resolved` with details of the added package, including dependencies on Swift Algorithms and Numerics packages.

--- Judo/ContentView.swift
+++ Judo/ContentView.swift
@@ -1,24 +1,58 @@
-//
-//  ContentView.swift
-//  Judo
-//
-//  Created by Jonathan Wight on 5/30/25.
-//
-
+import Observation
 import SwiftUI
+import Everything
 
 struct ContentView: View {
+
+    @State
+    var repository = Repository()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Hello, World!")
     }
 }
 
 #Preview {
     ContentView()
+}
+
+@Observable
+class Repository {
+    var path: FSPath = "/Users/schwa/Desktop/Judo"
+
+    var binaryPath: FSPath = "/opt/homebrew/bin/jj"
+
+    init() {
+        Task {
+
+            let template = """
+            "{\\n"
+            ++ "\t'commit_id': " ++ "'" ++ commit_id.shortest() ++ "|" ++ commit_id ++ "'" ++ ",\\n"
+            ++ "\t'author.name': " ++ author.name().escape_json() ++ ",\\n"
+            ++ "\t'author.email': '" ++ concat(author.email().local(), "@", author.email().domain()) ++ "',\\n"
+            ++ "\t'description': " ++ description.escape_json() ++ ",\\n"
+            ++ "\t'parents': " ++ parents.map(|c| c.commit_id()).join("|") ++ ",\\n"
+            ++ "},\\n"
+            """
+            .replacingOccurrences(of: "'", with: "\\\"")
+
+
+            let arguments = ["log", "--no-graph", "--reversed", "--template", template]
+            do {
+                let output = try Process.checkOutput(launchPath: binaryPath.path, arguments: arguments, currentDirectoryURL: path.url)
+
+                print(try! output.standardOutput!.readString())
+            }
+            catch {
+//                print("Error: \(error)")
+            }
+        }
+
+    }
+}
+
+extension Process.Output {
+    func readString() throws -> String {
+        String(data: try read(), encoding: .utf8)!
+    }
 }
