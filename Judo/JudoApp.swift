8cfc1e9605cbd01700792aecd79838e74a8489d5
Jonathan Wight <jwight@mac.com>
2025-05-30 09:41:36 -0700

    Initial Commit

---
+++ Judo/JudoApp.swift
@@ -0,0 +1,17 @@
+//
+//  JudoApp.swift
+//  Judo
+//
+//  Created by Jonathan Wight on 5/30/25.
+//
+
+import SwiftUI
+
+@main
+struct JudoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
