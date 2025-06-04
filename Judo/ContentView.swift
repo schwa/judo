fba784b2410ddb91071bc9a8fb45b1fd0e4c3d99
Jonathan Wight <jwight@mac.com>
2025-06-04 09:17:15 -0700

    Enhance commit display and configuration handling
    - Re-enabled and improved commit display in List view by organising commit details in a VStack.
    - Updated the Repository path and binary configuration to optimise development environment.
    - Introduced a structured encoding of temporary configuration files using a dedicated Template struct.
    - Extended commit information with additional fields like root, empty, bookmarks, and timestamp.
    - Refactored author details into a Signature structure, improving readability and maintainability of code.
    - Enhanced JSON decoding by allowing ISO 8601 formatted dates.

--- Judo/ContentView.swift
+++ Judo/ContentView.swift
@@ -9,21 +9,37 @@ struct ContentView: View {
     var repository = Repository()
 
     var body: some View {
-//        TemplateDemoView()
+        //        TemplateDemoView()
         List(repository.commits, id: \.commit_id) { commit in
-            HStack {
-                ChangeIDView(changeID: commit.change_id)
-                Text(commit.author_name)
-                    .foregroundStyle(.secondary)
-                Text(commit.author_email)
-                    .foregroundStyle(.secondary)
-                Spacer()
-                Text(commit.description)
-                    
+            VStack(alignment: .leading) {
+                HStack {
+                    ChangeIDView(changeID: commit.change_id).monospaced()
+                    if let email = commit.author.email {
+
+                        Text(email)
+                    }
+                    Text(commit.author.timestamp, style: .relative)
+                        .foregroundStyle(.cyan)
+                    if commit.bookmarks.isEmpty == false {
+                        Text("\(commit.bookmarks.joined(separator: ", "))")
+                            .foregroundStyle(.purple)
+                    }
+                    if commit.root {
+                        Text("root()").italic()
+                            .foregroundStyle(.green)
+                    }
+                    Text(commit.commit_id)
+                }
+                if commit.description.isEmpty {
+                    Text("(no description set").italic()
+                }
+                else {
+                    Text(commit.description.trimmingCharacters(in: .whitespacesAndNewlines))
+                }
             }
 
-
         }
+
     }
 }
 
@@ -33,7 +49,8 @@ struct ContentView: View {
 
 @Observable
 class Repository {
-    var path: FSPath = "/Users/schwa/Projects/Ultraviolence"
+    //    var path: FSPath = "/Users/schwa/Projects/Ultraviolence"
+    var path: FSPath = "/Users/schwa/Desktop/judo"
 
     var binaryPath: FSPath = "/opt/homebrew/bin/jj"
 
@@ -42,15 +59,24 @@ class Repository {
     init() {
         Task {
 
-            let temporaryConfig = JujutsuConfig(templateAliases: ["JudoCommitRecord": CommitRecord.template])
+            let temporaryConfig = JujutsuConfig(templateAliases: [
+                CommitRecord.template.key: CommitRecord.template.content,
+                Signature.template.key: Signature.template.content,
+            ])
             // write to temp directory
-            let tempDirectory = FileManager.default.temporaryDirectory
-            let tempConfigPath = tempDirectory.appending(path: "judo_jj_config.toml")
+            //            let tempDirectory = FileManager.default.temporaryDirectory
+            let tempDirectory = URL(fileURLWithPath: "/tmp")
+            let tempConfigPath = tempDirectory.appending(path: "judo.toml")
             print(tempConfigPath)
 
             let d = try TOMLEncoder().encode(temporaryConfig).write(toFile: tempConfigPath.path, atomically: true, encoding: .utf8)
 
-            let arguments = ["log", "--no-graph", "-r", "all()", "--template", "JudoCommitRecord", "--config-file", tempConfigPath.path]
+            let arguments = ["log", "--no-graph",
+                             "-r", "all()",
+                             "--template", CommitRecord.template.name,
+                             "--config-file", tempConfigPath.path,
+                             //                "--limit", "1"
+            ]
             print("jj \(arguments.joined(separator: " "))")
             do {
                 print("Fetching...")
@@ -64,16 +90,17 @@ class Repository {
                 let footer = "\n]".data(using: .utf8)!
                 let jsonData = header + data + footer
                 let jsonString = String(data: jsonData, encoding: .utf8)!
-//                print(jsonString)
+                print(jsonString)
 
                 let decoder = JSONDecoder()
                 decoder.allowsJSON5 = true
+                decoder.dateDecodingStrategy = .iso8601
 
 
 
                 self.commits = try decoder.decode([CommitRecord].self, from: jsonData)
                 print("Done")
-//                print(try! output.standardOutput!.readString())
+                //                print(try! output.standardOutput!.readString())
             }
             catch {
                 print("Error: \(error)")
@@ -136,28 +163,59 @@ struct JujutsuConfig: Codable {
 
 // https://jj-vcs.github.io/jj/latest/templates/
 
+struct Template {
+    var name: String
+    var parameters: [String] = []
+    var content: String
+
+    var key: String {
+        return name + (parameters.isEmpty ? "" : "(\(parameters.joined(separator: ",")))")
+    }
+}
+
+struct Signature: Decodable {
+    var name: String
+    var email: String?
+    var timestamp: Date
+
+    static let template = Template(name: "JUDO_SIGNATURE", parameters: ["p"], content: """
+        "{"
+        ++ "'name': " ++ p.name().escape_json()
+        ++ ", " ++ "'email': '" ++ p.email().local() ++ "@" ++ p.email().domain() ++ "'"
+        ++ ", 'timestamp': '" ++ p.timestamp().format("%Y-%m-%dT%H:%M:%S%z")
+        ++ "'}"
+        """.replacingOccurrences(of: "'", with: "\\\"")
+    )
+}
+
+
 struct CommitRecord: Identifiable, Decodable {
 
     var id: String { commit_id }
 
     var change_id: ChangeID
     var commit_id: String
-    var author_name: String
-    var author_email: String
+    var author: Signature
     var description: String
     var parents: [String]
+    var root: Bool
+    var empty: Bool
+    var bookmarks: [String]
 
-    static let template = """
+    static let template = Template(name: "judo_commit_record", content: """
         "{\\n"
         ++ "\t'change_id': " ++ "'[" ++ change_id.shortest() ++ "]" ++ change_id ++ "'" ++ ",\\n"
         ++ "\t'commit_id': " ++ "'[" ++ commit_id.shortest() ++ "]" ++ commit_id ++ "'" ++ ",\\n"
-        ++ "\t'author_name': " ++ author.name().escape_json() ++ ",\\n"
-        ++ "\t'author_email': '" ++ concat(author.email().local(), "@", author.email().domain()) ++ "',\\n"
+        ++ "\t'author': " ++ JUDO_SIGNATURE(author) ++ ",\\n"
         ++ "\t'description': " ++ description.escape_json() ++ ",\\n"
+        ++ "\t'root': " ++ root ++ ",\\n"
+        ++ "\t'empty': " ++ empty ++ ",\\n"
         ++ "\t'parents': [" ++ parents.map(|c| "'" ++ c.commit_id() ++ "'").join("|") ++ "],\\n"
+        ++ "\t'bookmarks': [" ++ bookmarks.map(|c| "'" ++ c ++ "'").join("|") ++ "],\\n"
         ++ "},\\n"
         """
         .replacingOccurrences(of: "'", with: "\\\"")
+    )
 }
 
 struct TemplateDemoView: View {
@@ -196,7 +254,7 @@ struct TemplateDemoView: View {
                 arguments.append(contentsOf: ["--template", template])
             }
 
-            let process = SimpleAsyncProcess(executableURL: URL(fileURLWithPath: "/opt/homebrew/bin/jj"), arguments: arguments, currentDirectoryURL: URL(fileURLWithPath: "/Users/schwa/Projects/Ultraviolence"))   
+            let process = SimpleAsyncProcess(executableURL: URL(fileURLWithPath: "/opt/homebrew/bin/jj"), arguments: arguments, currentDirectoryURL: URL(fileURLWithPath: "/Users/schwa/Projects/Ultraviolence"))
             do {
                 let data = try await process.run()
                 output = String(data: data, encoding: .utf8) ?? "Failed to decode output"
